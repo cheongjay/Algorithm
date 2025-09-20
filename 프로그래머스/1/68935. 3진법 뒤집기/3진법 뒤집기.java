b42510b07967db5c5fdba9d2cdd1aedd027af6af
JJ <113548427+cheongjay@users.noreply.github.com>
2025-09-20 22:52:47 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.10 ms, Memory: 68.2 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -2,40 +2,20 @@ import java.util.*;
 
 class Solution {
     public int solution(int n) {
+        
+        StringBuilder a = new StringBuilder();
+        while(n!=0){
+            a.append(n%3);
+            n = n/3;
+        }
+        
+        String base3 = a.reverse().toString();
+        
         int answer = 0;
-        int[] numsBase3 = new int[17];
-        
-        // 10진법 -> 3진법
-        for(int i = 16; i >= 0; i--){
-            for(int j = 2; j >= 0 ; j--){
-                int m = (int)(Math.pow(3,i))  * j;
-                if(m <= n){
-                    numsBase3[i] = j;
-                    n -= m;
-                    break;
-                }
-            }    
+        for(int i = 0 ; i < base3.length(); i++){            
+            answer += Math.pow(3,i) * (base3.charAt(i)-'0');
         }
         
-        List<Integer> reversed = new ArrayList<>();
-        
-        int j =0;
-        Boolean flag = true; // 필요없는 윗자리 0 제거용 플래그
-        
-        for(int i = 16; i >= 0; i--){
-            if(numsBase3[i] == 0 && flag)
-                continue;
-            reversed.add(numsBase3[i]);
-            flag = false;
-        }
-
-        int i = 0;
-        for(int r : reversed){
-            int add= (int)(Math.pow(3,i)) * r;
-            i++;
-            answer += add;
-        }
-
         return answer;
     }
 }
