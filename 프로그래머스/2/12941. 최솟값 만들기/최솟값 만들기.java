b82b78c4444c30550dd5a85623de8f43f62e9447
JJ <113548427+cheongjay@users.noreply.github.com>
2025-09-27 22:06:11 +0900

    [level 2] Title: 최솟값 만들기, Time: 4.93 ms, Memory: 53.6 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -4,33 +4,33 @@ class Solution
     public int solution(int []A, int []B)
     {
         int answer = 0;
-//         Integer[] aRef = new Integer[A.length];
-//         Integer[] bRef = new Integer[B.length];
+        Integer[] aRef = new Integer[A.length];
+        Integer[] bRef = new Integer[B.length];
         
-//         for(int i = 0; i< A.length; i++){
-//             aRef[i] = A[i];
-//             bRef[i] = B[i];
-//         }
-//         List<Integer> aList = Arrays.asList(aRef);
-//         List<Integer> bList = Arrays.asList(bRef);
-        
-//         Collections.sort(aList);
-//         Collections.sort(bList);
-//         Collections.reverse(bList);
-//         System.out.println(aList);
-//         System.out.println(bList);
-        
-//         for(int i = 0; i < aList.size(); i++){
-//             answer += aList.get(i) * bList.get(i);
-//         }
-        Arrays.sort(A);
-        Arrays.sort(B);
-
-        int bIndex = A.length -1;
-        
-        for(int i = 0; i < A.length; i++){
-            answer += A[i] * + B[bIndex - i];
+        for(int i = 0; i< A.length; i++){
+            aRef[i] = A[i];
+            bRef[i] = B[i];
         }
+        List<Integer> aList = Arrays.asList(aRef);
+        List<Integer> bList = Arrays.asList(bRef);
+        
+        Collections.sort(aList);
+        Collections.sort(bList);
+        // Collections.reverse(bList);
+        // System.out.println(aList);
+        // System.out.println(bList);
+        int size = aList.size() - 1;
+        for(int i = 0; i < aList.size(); i++){
+            answer += aList.get(i) * bList.get(size -i);
+        }
+//         Arrays.sort(A);
+//         Arrays.sort(B);
+
+//         int bIndex = A.length -1;
+        
+//         for(int i = 0; i < A.length; i++){
+//             answer += A[i] * + B[bIndex - i];
+//         }
         return answer;
     }
 }
