abb5c13a202016d3e503ad968a0eaf36ce399398
JJ <113548427+cheongjay@users.noreply.github.com>
2026-01-03 21:27:56 +0900

    [level 2] Title: 주식가격, Time: 21.48 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int[] prices) {
+        int len = prices.length;
+        int[] answer = new int[len];
+        
+        for(int i = 0; i < len - 1; i++){
+            int count = 0;
+
+            for(int j = i+1; j < len; j++){
+                count++;
+                if(prices[i] > prices[j]){
+                    break;
+                }
+            }
+            answer[i] = count;
+        }
+        return answer;
+    }
+}
