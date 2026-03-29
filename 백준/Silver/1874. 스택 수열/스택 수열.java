909265211d8645d11ed3ab1f46ea9b9a64ff0c98
JJ <113548427+cheongjay@users.noreply.github.com>
2026-03-29 22:07:12 +0900

    [Silver II] Title: 스택 수열, Time: 316 ms, Memory: 29024 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -6,42 +6,33 @@ public class Main
 	public static void main(String[] args) throws IOException{
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    
-	    int N = Integer.parseInt(br.readLine());
+	    int n = Integer.parseInt(br.readLine());
 	    Stack<Integer> s = new Stack<>();
-	    
-	    s.push(1);
-	    int num = 1; // 1~N까지 오름차순 삽입할 숫자
-	    int next = Integer.parseInt(br.readLine());
+	    int input;
+	    int start = 0;
 	    StringBuilder sb = new StringBuilder();
-	    sb.append("+");
-	    int processed = 0; // 처리한 숫자 카운트
 	    
-	    while(!s.isEmpty() || num < N){
-	        // 1~N까지 다 삽입했는데, peek가 다음 수열 숫자랑 다르면 못 만들기 때문에, No 출력하고 break
-	        if(num > N){                         
-	            if(s.peek() != next){
-	                System.out.println("NO");
-	                return;
-	            }
+	    for(int i = 0; i < n; i++){
+	        input = Integer.parseInt(br.readLine());
+	        if(start < input){
+    	        for(int j = start + 1; j <= input; j++){
+    	            s.push(j);
+    	            sb.append("+").append("\n");
+    	        }
+    	        
+    	        start = input;
 	        }
-	        // 동일하지 않으면 계속 삽입
-	        if(s.isEmpty() || s.peek() != next){
-	            s.push(++num);
-	            sb.append("+");
-	        } 
-	        // 만약 뽑는 숫자가 수열 숫자랑 동일하면 팝
-	        else{
-	            s.pop();
-	            sb.append("-");
-	            processed++;
-	            if(processed < N)
-    	            next = Integer.parseInt(br.readLine());
+	        
+	        else if(s.peek() != input){
+	            System.out.println("NO");
+	            return;
 	        }
+	        
+	        s.pop();
+	        sb.append("-").append("\n");
 	    }
 	    
-	    String[] print = sb.toString().split("");
-	    for(String p : print){
-	        System.out.println(p);
-	    }
+	    System.out.println(sb.toString());
+	   
 	}
 }
