1e111144f11d70277a8080b5d7354f36e91fdb41
JJ <113548427+cheongjay@users.noreply.github.com>
2026-05-07 18:24:59 +0900

    [level 2] Title: 더 맵게, Time: 918.58 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
 
 ### 성능 요약
 
-메모리: 129 MB, 시간: 1432.42 ms
+메모리: 132 MB, 시간: 918.58 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 23일 22:52:49
+2026년 05월 07일 18:24:55
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -2,26 +2,25 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] scoville, int K) {
-        int answer = 0;
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<>(
-            (s1, s2) ->{
-                return s1 - s2;            
-        });
-        
+        // min heap으로 스코빌 지수를 저장하고
+        // min 값이 k 이상이 될 때까지
+            // 첫번째 값과 두 번째 값 꺼내서 새로운 음식 만들고 집어넣기
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> a - b);
         for(int s : scoville){
-            pq.add(s);
+            pq.offer(s);
         }
         
+        int answer = 0;
+        
         while(pq.peek() < K){
+            int first = pq.poll();
             
-            if(pq.size() < 2){
-                return -1;
-            }
-            
-            int lowest = pq.poll();
-            int lowest2nd = pq.poll();
-            pq.add(lowest + lowest2nd * 2);
+            // 더이상 없으면 만들 수 없는 경우임
+            if(pq.isEmpty())
+               return -1;
+               
+            int second = pq.poll();
+            pq.offer(first + second * 2);
             answer++;
         }
         
